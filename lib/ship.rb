24eed02091da9b24dd1be34a38653bdb5613af69
Lisa A-R <lisaar@gmail.com>
2014-07-19 12:09:05 +0100

    ship class coded

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,28 @@
 class Ship
+
+	def initialize(coordinates)
+		@coordinates = coordinates
+		@hits = []
+	end
+
+	def has_coordinates?
+		@coordinates.any?
+	end
+
+	def length
+		@coordinates.count
+	end
+
+	def shot_at(coordinate)
+		@hits << coordinate
+		self
+	end
+
+	def has_hits?
+		@hits.any?
+	end
+
+	def sunk?
+		 @hits.length == length
+	end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,5 +1,31 @@
 require 'ship'
 
 describe Ship do
+
+	let(:ship) {Ship.new([:A1, :A2])}
+
+	it 'intializes ships with coordinates' do
+		expect(ship).to have_coordinates
+	end
+
+	it 'has a length' do
+		expect(ship.length).to eq 2
+		ship = Ship.new([:A1, :A2, :A3])
+		expect(ship.length).to eq 3
+	end
+
+	it 'has hits when ship is shot at a certain coordinate' do
+		expect(ship.shot_at(:A1)).to have_hits
+	end
+
+	it 'cannot be sunk first initalized' do
+		expect(ship).not_to be_sunk
+	end
+
+	it 'can be sunk when all ships coordinates have been hit' do
+		ship.shot_at(:A1)
+		ship.shot_at(:A2)
+		expect(ship).to be_sunk
+	end
 	
 end
